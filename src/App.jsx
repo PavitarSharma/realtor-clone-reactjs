7f2e99a45db2135452d30f0758966ee7b5d8d163
PavitarSharma <pavitarsharma144@gmail.com>
2023-02-13 10:38:52 +0530

    app set up with react vite react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import ForgotPassword from "./pages/ForgotPassword"
+import Offer from "./pages/Offer"
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/offers" element={<Offer />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
